80dd3a48cf67452c8241749af76756dfb7c99a74
Bryan <bryan.brick@amtote.com>
2021-07-24 22:33:55 -0400

    set up a basic webpack server

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Mooooo!',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
